8f36b59c8514553d1acee548a65a99e51cbe02e8
Kaviilee <jiaweilee95@126.com>
2023-10-11 18:46:07 +0800

    ci: update build script

--- gulpfile.prod.js
+++ gulpfile.prod.js
@@ -8,62 +8,62 @@ const minify = require('gulp-minify');
 gulp.task('css', function () {
   return gulp.src('src/css/*.css')
     .pipe(concat('all.css'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('svg-editor'));
 });
 
 gulp.task('js', function () {
   return gulp.src(['src/js/*.js', 'src/lib/*.js']) 
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('svg-editor'));
 });
 
 gulp.task('loading', function () {
   return gulp.src('src/js/loading.js')
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('svg-editor'));
 });
 
 gulp.task('index', function () {
   return gulp.src('src/*.html')
     .pipe(useref())
     .pipe(cachebust({type: 'timestamp'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('svg-editor'));
 });
 
 // no service worker implemented yet
 gulp.task('cache', function(){
   return gulp.src(['./src/serviceworker.js'])
     .pipe(replace('<timestamp>', Date.now()))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./svg-editor/'));
 });
 
 gulp.task('manifest', function(){
   return gulp.src(['./src/site.webmanifest'])
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./svg-editor/'));
 });
 
 gulp.task('images', function(){
   return gulp.src(['src/images/**/*'])
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest('svg-editor/images'));
 });
 
 gulp.task('extensions', function(){
   return gulp.src(['src/extensions/**/*'])
-    .pipe(gulp.dest('dist/extensions'));
+    .pipe(gulp.dest('svg-editor/extensions'));
 });
 
 gulp.task('shapelib', function(){
   return gulp.src(['src/shapelib/**/*'])
-    .pipe(gulp.dest('dist/shapelib'));
+    .pipe(gulp.dest('svg-editor/shapelib'));
 });
 
 gulp.task('canvg', function(){
   return gulp.src(['src/js/lib/canvg.js', 'src/js/lib/rgbcolor.js'])
-    .pipe(gulp.dest('dist/js/lib'));
+    .pipe(gulp.dest('svg-editor/js/lib'));
 });
 
 gulp.task('i18n', function(){
   return gulp.src(['src/locales/**/*'])
-    .pipe(gulp.dest('dist/locales'));
+    .pipe(gulp.dest('svg-editor/locales'));
 });
 
 gulp.task('build', 
